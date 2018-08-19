c0ce34f1312e58c1f0578d643408d74e5cf4abf1
John Spurney <spurneyjohn@gmail.com>
2018-08-19 14:21:37 -0400

    Add event listeners to gui elements

--- js/simulator.js
+++ js/simulator.js
@@ -1,3 +1,18 @@
+const NUMBER_OF_MAPS = 8;
+// A flag to keep track of when the simulation is running
+let simulationStarted = false;
+// Path entries are in the following format: [xTrailer, yTrailer, thetaTrailer]
+let path = [];
+// Flag that keeps track of when the pathbrush is being used
+let pathBrushActivated = false;
+let currentPathIndex = 0;
+let waypointDistanceThreshold = 14;
+let startSimButton = document.getElementById('startSimButton');
+let clearPathButton = document.getElementById('clearPathButton');
+let canvasDynamic = document.getElementById('canvasDynamic');
+let pathbrushStatus = document.getElementById('pathbrushStatus');
+let mapSelect = document.getElementById('mapSelect');
+
 let tractorTrailerSettings = {
   lengthTractor: 23,
   lengthHitch: 5,
@@ -15,6 +30,46 @@ let tractorTrailerSettings = {
   vehicleColor: 'rgba(21, 88, 116, 0.7)',
   hitchColor: 'OrangeRed'
 };
+
+function initializeMapSelect() {
+  let optionElement = null;
+
+  for (let i = 0; i < NUMBER_OF_MAPS; ++i) {
+    optionElement = document.createElement('option');
+    optionElement.innerHTML = i.toString();
+    mapSelect.appendChild(optionElement);
+  }
+}
+initializeMapSelect();
+
+startSimButton.addEventListener('click', function() {
+  if (simulationStarted) {
+    // TODO: Start the simulation here
+    simulationStarted = false;
+    this.innerHTML = 'Start Simulation';
+  } else {
+    // TODO: Stop the simulation here
+    simulationStarted = true;
+    this.innerHTML = 'Stop Simulation';
+  }
+});
+
+clearPathButton.addEventListener('click', function() {
+  // TODO: Clear the canvas here
+  path = [];
+  currentPathIndex = 0;
+  // TODO: the waypoint index from the simulation will be zero here aswell
+});
+
+canvasDynamic.addEventListener('mousedown', function() {
+  pathBrushActivated = true;
+  pathbrushStatus.innerHTML = 'On';
+});
+
+canvasDynamic.addEventListener('mouseup', function() {
+  pathBrushActivated = false;
+  pathbrushStatus.innerHTML = 'Off';
+});
 
 let tractorTrailer = new TractorTrailer(...Object.values(tractorTrailerSettings));
 console.log(tractorTrailer);
